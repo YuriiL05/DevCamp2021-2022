4c09f94c394e4e666be7d2e0f25832eeff536b06
Yurii Lykhitskyi <lykhitskyi@gmail.com>
2022-01-07 23:00:18 +0200

    [api] Add Users routing

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./services/db');
 
 const config = require('./services/config');
+const userRouters = require('./routes/users');
 
 const port = config.appPort;
 
@@ -11,49 +11,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/', async (req, res) => {
-  try {
-    await db('Universities').insert(req.body);
-  } catch (e) {
-    console.log(e);
-  }
-
-  res.send(req.body);
-});
-
-app.delete('/:id', async (req, res) => {
-  try {
-    await db('Universities').where({ UniversityID: req.params.id }).delete();
-  } catch (e) {
-    console.log(e);
-  }
-
-  res.send();
-});
-
-app.get('/', async (req, res) => {
-  let result = '1';
-
-  try {
-    result = await db.select().from('Universities');
-  } catch (e) {
-    console.log(e);
-  }
-
-  res.send(result);
-});
-
-app.put('/:id', async (req, res) => {
-  try {
-    await db('Universities')
-      .where({ UniversityID: req.params.id })
-      .update(req.body);
-  } catch (e) {
-    console.log(e);
-  }
-
-  res.send();
-});
+app.use('/users', userRouters);
 
 app.listen(port, () => {
   console.log(`Started at port ${port}`);
---
+++ api/src/routes/users.js
@@ -0,0 +1,92 @@
+const router = require('express').Router();
+const db = require('../services/db');
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await db.select().from('Users').timeout(10000);
+
+    res.status(200).send(users);
+  } catch (e) {
+    res.status(500).send({ error: 'Users cannot be loaded', e });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  if (Number.isInteger(+id)) {
+    try {
+      const user = await db('Users').where({ UserID: id });
+      if (user.length > 0) {
+        res.status(200).send(user);
+      } else {
+        res.status(404).send({ error: `User with Id: ${id} is not found` });
+      }
+    } catch (e) {
+      res.status(500).send({ error: 'User cannot be loaded', e });
+    }
+  } else {
+    res.status(400).send({ error: 'User Id is not correct' });
+  }
+});
+
+router.post('/', async (req, res) => {
+  const newUserData = req.body;
+
+  if (Object.keys(newUserData).length > 0) {
+    try {
+      await db('Users').insert(newUserData);
+
+      res.status(200).send('New user was added');
+    } catch (e) {
+      res.status(500).send({ error: 'User cannot be added', e });
+    }
+  } else {
+    res.status(400).send({ error: 'User information is empty' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const id = req.params.id;
+  const userUpdates = req.body;
+
+  if (Number.isInteger(+id)) {
+    try {
+      const user = await db('Users').where({ UserID: id });
+      if (user.length > 0) {
+        const updatedInfoUser = { ...user[0], ...userUpdates };
+
+        await db('Users').where({ UserID: id }).update(updatedInfoUser);
+        res.status(200).send(updatedInfoUser);
+      } else {
+        res.status(404).send({ error: `User with Id: ${id} is not found` });
+      }
+    } catch (e) {
+      res.status(500).send({ error: 'User cannot be loaded', e });
+    }
+  } else {
+    res.status(400).send({ error: 'User Id is not correct' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  if (Number.isInteger(+id)) {
+    try {
+      const isDeleted = await db('Users').where({ UserID: id }).delete();
+
+      res.send(
+        isDeleted
+          ? `User with Id: ${id} is deleted`
+          : `User with Id: ${id} is absent`
+      );
+    } catch (e) {
+      res.status(500).send({ error: 'User cannot be deleted', e });
+    }
+  } else {
+    res.status(400).send({ error: 'User Id is not correct' });
+  }
+});
+
+module.exports = router;
