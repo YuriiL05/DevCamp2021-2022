6918bfbc6a5463a2c0248f548115371d0ea5dc86
Yurii Lykhitskyi <lykhitskyi@gmail.com>
2022-02-23 03:02:45 +0200

    [front] Add user context

--- front/src/containers/body/index.js
+++ front/src/containers/body/index.js
@@ -9,27 +9,33 @@ import { AddArticleContainer } from "../articles/addArticle";
 import { useQuery } from "react-query";
 import { getAccessLevels } from "../articles/api/crud";
 import { Loading } from "../../components/loading";
+import userContext from "../../contexts/userContext";
 
 export const BodyContainer = () => {
   const [openArtAdd, setOpenArtAdd] = useState(false);
+  const [userData, setUserData] = useState({authenticated: false, user: null, setUserData: () => {}});
 
   const { isFetching, refetch, data, isFetched } = useQuery('accessLevels', () => getAccessLevels());
   const accessLevels = data?.data;
 
-   return (
+  console.log(userData);
+
+  return (
     <>
-      <HeaderContainer setOpenArtAdd={setOpenArtAdd}/>
-      <div className={"bodyContent"}>
-        <div className={"content"}>
-          <ErrorBoundary>
-            {isFetching && <Loading/>}
-            {isFetched && openArtAdd && <AddArticleContainer openArtAdd={openArtAdd}
-                                                             setOpenArtAdd={setOpenArtAdd}
-                                                             accessLevels={accessLevels}/>}
-            {isFetched && <Outlet context={accessLevels}/>}
-          </ErrorBoundary>
+      <userContext.Provider value={userData}>
+        <HeaderContainer setOpenArtAdd={setOpenArtAdd}/>
+        <div className={"bodyContent"}>
+          <div className={"content"}>
+            <ErrorBoundary>
+              {isFetching && <Loading/>}
+              {isFetched && openArtAdd && <AddArticleContainer openArtAdd={openArtAdd}
+                                                               setOpenArtAdd={setOpenArtAdd}
+                                                               accessLevels={accessLevels}/>}
+              {isFetched && <Outlet context={accessLevels}/>}
+            </ErrorBoundary>
+          </div>
         </div>
-      </div>
+      </userContext.Provider>
     </>
   );
 };
